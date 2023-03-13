1f3f04fb9e80fa79d24808c009fdc7d090947a29
L-cir94 <luca.cirigliano94@hotmail.it>
2023-03-13 20:04:55 +0100

    add comment for const userMail

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -9,7 +9,7 @@ in pagina.
 
 */
 //crea lista di chi può accedere
- const userList =[
+const userList = [
   'luca.cirigliano@gmail.com',
   'mimmo@gmail.com',
   'pippo@gmail.com',
@@ -17,28 +17,42 @@ in pagina.
   'riccio@gmail.com'
 ]
 //chiedi utente la sua email
-//const userMail = prompt('inserisci la tua email')//per es normale 
-const userMail = document.getElementById("mail");//per bonus
-const access = document.getElementById("access")
+
+/* const userMail = prompt('inserisci la tua email'); */ /*togli il commento per far leggere questa CONST
+e commenta l'altra const userMail*/
+
+const userMail = document.getElementById("mail"); /*togli il commento per far leggere questa CONST
+e commenta l'altra const userMail*/
+const access = document.getElementById("access");
 const button = document.querySelector('button');
-button.addEventListener('click', function(){
+button.addEventListener('click', function () {
+  
   let inputUserMail = userMail.value
   console.log(inputUserMail);
   let hasaccess = false
   for (let i = 0; i < userList.length; i++)
-//controlla che sia nella lista delgi ammessi 
-if (inputUserMail == userList[i]){
-  console.log('accesso consentito')
-  i = userList.length;
-  hasaccess =true}
-  if (hasaccess){
+    //controlla che sia nella lista delgi ammessi 
+    if (inputUserMail == userList[i]) {
+
+      //mostra in console e in HTML il messaggio di accesso/non accesso
+      console.log('accesso consentito')
+      i = userList.length;
+      hasaccess = true
+    }
+  if (hasaccess) {
+
+   //mostra in console e in HTML il messaggio di accesso/non accesso
     access.innerHTML = 'Accesso consentito'
-  }else {
-console.log('accesso negato')
-//stampa un messaggio su HTML
-access.innerHTML = 'Accesso non consentito'
-}
+  } else {
+
+    console.log('accesso negato')
+    //stampa un messaggio su HTML
+    access.innerHTML = 'Accesso non consentito'
+  }
 })
+
+
+
 /* (2)Gioco dei dadi
 Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 Stabilire il vincitore, in base a chi fa il punteggio più alto.
@@ -53,12 +67,15 @@ console.log(`numero utente ${numberUser} numero pc ${numberPc} `)
 const diceEl = document.getElementById("computersays")
 //Stabilire il vincitore, in base a chi fa il punteggio più alto.
 if (numberUser > numberPc) {
-    console.log("hai vinto")
-    diceEl.innerHTML = `${numberPc}<--pc<br> ${numberUser}<--il tuo numero <br> hai vinto`
+
+  console.log("hai vinto")
+  diceEl.innerHTML = `${numberPc}<--pc<br> ${numberUser}<--il tuo numero <br> hai vinto`
 } else if (numberUser == numberPc) {
-    console.log("hai pareggiato") 
-    diceEl.innerHTML =`${numberPc}<--pc<br> ${numberUser}<--il tuo numero <br> hai pareggiato`
+
+  console.log("hai pareggiato")
+  diceEl.innerHTML = `${numberPc}<--pc<br> ${numberUser}<--il tuo numero <br> hai pareggiato`
 } else {
-    console.log("hai perso");
-    diceEl.innerHTML =`${numberPc}<--pc<br> ${numberUser}<--il tuo numero <br> hai perso`
+
+  console.log("hai perso");
+  diceEl.innerHTML = `${numberPc}<--pc<br> ${numberUser}<--il tuo numero <br> hai perso`
 }
